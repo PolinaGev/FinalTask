fa7887016c79d8bafa6a732ed4e079d303531624
PolinaG <PolinaG@gmail.com>
2022-10-03 18:34:36 +0300

    first commit

---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array = new string[3] {"///", "cat", "12345"};
+string[] array2 = new string[array.Length];
+void FinishedArray(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        array2[count] = array[i];
+        count++;
+        }
+    }
+}
+void WriteArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FinishedArray(array, array2);
+WriteArray(array2);
